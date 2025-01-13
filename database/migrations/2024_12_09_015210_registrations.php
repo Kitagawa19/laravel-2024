65bedf88246751d3b1881f0a55baab2c395cc8ea
Kitagawa19 <kkitagawa16219@gmail.com>
2025-01-13 15:24:25 +0900

    add 授業の開始と終了時刻を追加

--- database/migrations/2024_12_09_015210_registrations.php
+++ database/migrations/2024_12_09_015210_registrations.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,18 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('registrations', function (Blueprint $table) {
-            $table->id(); 
-            $table->unsignedBigInteger('user_id')->index(); 
-            $table->unsignedBigInteger('subject_id')->index(); 
+            $table->id();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->unsignedBigInteger('subject_id')->index();
+            $table->unsignedTinyInteger('start_period'); 
+            $table->unsignedTinyInteger('end_period');  
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
 
-            // 外部キー制約
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
                 ->onUpdate('cascade')
-                ->onDelete('cascade'); // 必要に応じて追加
+                ->onDelete('cascade'); 
 
             $table->foreign('subject_id')
                 ->references('id')
