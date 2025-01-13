9d4128c436b52b80a3dd4a7ec403918bcc9cd7e8
Kitagawa19 <kkitagawa16219@gmail.com>
2025-01-13 15:23:21 +0900

    add 履修登録した科目の表示

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,15 +6,48 @@
     </x-slot>
 
     <div class="py-12">
-    <div class="flex items-center mt-4">
-            <x-primary-button class="ms-3" onClick="location.href='{{route('register.show')}}' ">
-                {{ __("登録") }}
-            </x-primary-button>
+        <div class="flex flex-row">
+            <div class="items-center mt-4">
+                <x-primary-button class="ms-3" onClick="location.href='{{route('register.getSubject')}}' ">
+                    {{ __("履修登録") }}
+                </x-primary-button>
+            </div>
+            <div class="items-center mt-4">
+                <x-primary-button class="ms-3" onClick="location.href='{{route('register.getSubject')}}' ">
+                    {{ __("変更") }}
+                </x-primary-button>
+            </div>
         </div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-6">
+                    <h3 class="font-semibold text-lg text-gray-800 mb-4">履修登録一覧</h3>
+                    <table class="min-w-full table-auto border-collapse border border-gray-300">
+                        <thead>
+                            <tr class="bg-gray-200">
+                                <th class="border border-gray-300 px-4 py-2 text-left">曜日</th>
+                                <th class="border border-gray-300 px-4 py-2 text-left">時間帯</th>
+                                <th class="border border-gray-300 px-4 py-2 text-left">科目名</th>
+                                <th class="border border-gray-300 px-4 py-2 text-left">担当教員</th>
+                                <th class="border border-gray-300 px-4 py-2 text-left">単位数</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($registrations as $registration)
+                                <tr>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $registration->subject->detail->date }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $registration->subject->detail->time }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $registration->subject->name }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $registration->subject->teacher->name }}</td>
+                                    <td class="border border-gray-300 px-4 py-2">{{ $registration->subject->detail->credit }}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5" class="border border-gray-300 px-4 py-2 text-center">履修登録データがありません。</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
