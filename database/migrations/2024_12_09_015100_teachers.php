4c9abd022923fe9587f5f379c5f5057220c778ba
Kitagawa19 <kkitagawa16219@gmail.com>
2025-01-13 10:51:28 +0900

    fix timestampを修正

--- database/migrations/2024_12_09_015100_teachers.php
+++ database/migrations/2024_12_09_015100_teachers.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->timestamps(); 
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
