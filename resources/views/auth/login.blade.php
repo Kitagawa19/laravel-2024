3761de308970b72f247a68bdcdfc9a633d10ce81
Kitagawa19 <kkitagawa16219@gmail.com>
2025-01-20 11:38:20 +0900

    add 新規作成リンクの追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,10 +38,15 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            <a class="underline  text-sm ms-3 text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Sign Up') }}
+            </a>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
+            <div class="flex justify-end mt-4">
+                <x-primary-button class="ms-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
     </form>
 </x-guest-layout>
